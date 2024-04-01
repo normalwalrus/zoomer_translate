608fac7a45a1d7bba557ba416fe1fdb2eb4b1092
ian <ianchanjiajun@gmail.com>
2024-04-01 14:36:00 +0800

    Added a thank you page, will change later

--- fronpage/urls.py
+++ fronpage/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.my_view),
+    path('submit/', views.submit_form, name='submit_form'),
+    path('thankyou/', views.thank_you, name='thank_you'),
 ]
--- fronpage/views.py
+++ fronpage/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+def submit_form(request):
+    if request.method == 'POST':
+        text_option = request.POST.get('text_option')
+        # Process the form data as needed
+        # Redirect to another page
+        return render(request, 'thankyou.html')  # Redirects to /thank-you URL
+    else:
+        return redirect('select_option')
 # Create your views here.
 
 def my_view(request):
     return render(request, 'frontpage.html')
+
+def thank_you(request):
+    return render(request, 'thankyou.html')
